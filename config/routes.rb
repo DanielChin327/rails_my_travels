049187fa67b1d82287fd65bd763fc32560a81556
Daniel Chin <DanChin327@gmail.com>
2024-04-18 21:47:35 +0900

    added country and posts routes, models, and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/edit'
+  get 'posts/create'
+  get 'posts/update'
+  get 'posts/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -7,4 +13,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  root 'countries#index'
+  resources :countries do
+    resources :posts
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# db/seeds.rb
+countries = ['Japan', 'Korea', 'Singapore']
+countries.each do |country_name|
+  Country.create(name: country_name)
+end

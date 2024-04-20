d81fe984e5128c33defbf51362cecee2b0a6376f
Daniel Chin <DanChin327@gmail.com>
2024-04-20 10:26:35 +0900

    working on posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,22 @@
 class PostsController < ApplicationController
+  before_action :set_country
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @posts = @country.posts
+  end
+
   def show
-    @posts=Post.all
   end
 
-  def new
+  # Additional methods for new, create, edit, update, destroy
+
+  private
+  def set_country
+    @country = Country.find(params[:country_id])
   end
 
-  def edit
-  end
-
-  def create
-  end
-
-  def update
-  end
-
-  def destroy
+  def set_post
+    @post = @country.posts.find(params[:id])
   end
 end
